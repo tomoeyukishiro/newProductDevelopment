c55fc30fadef2fbaceb0fcc8f63557dc01f3e8c9
Peter Cottle <petermcottle@gmail.com>
2012-10-10 22:38:50 -0700

    basic hello world in node up

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ newProductDevelopment
 Repo for the new product development site. Steps to set up locally:
 
 - Install node
-- Install express
+- Install dependencies by going into /src/ and doing "npm install"
--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-var app = require('express').createServer();
+var express = require('express');
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+var app = express(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3600;
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
 });
 
