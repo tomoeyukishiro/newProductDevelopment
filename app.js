105348618fee49092e3238618771b167f139aaaa
Peter Cottle <petermcottle@gmail.com>
2012-10-10 23:38:24 -0700

    templating engine downgit status

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
 var express = require('express');
-var app = express.createServer(express.logger());
+var mustache = require('./lib/mustache.js');
+var tmpl = require('./lib/tmpl.js');
 
+var tmpl = {
+    compile: function (source, options) {
+        if (typeof source == 'string') {
+            return function(options) {
+                options.locals = options.locals || {};
+                options.partials = options.partials || {};
+                if (options.body) // for express.js > v1.0
+                    locals.body = options.body;
+                return mustache.to_html(
+                    source, options.locals, options.partials);
+            };
+        } else {
+            return source;
+        }
+    },
+    render: function (template, options) {
+        template = this.compile(template, options);
+        return template(options);
+    }
+};
+
+var app = express.createServer();
 app.configure('development', function() {
+  // errors
+  app.use(express.logger());
   app.use(express.errorHandler({
     dumpExceptions: true,
     showStack: true
   }));
+
+  // Views configuration!
+  app.use(express.methodOverride());
+  //app.use(express.bodyDecoder());
+  app.use(app.router);
+
+  app.set('views', __dirname + '/views');
+  app.set('view options', {layout: false});
+  app.register('.html', tmpl);
 });
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index.html', {
+    locals: {
+      title: 'New Product Development!'
+    }
+  });
+
+  // response.send('Hello World!');
 });
 
 var port = process.env.PORT || 3600;
