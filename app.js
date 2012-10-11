959c3ba6688022c9d3a0d025ee2b5f12be653b81
Peter Cottle <petermcottle@gmail.com>
2012-10-10 23:14:40 -0700

    showing error traces

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
+app.configure('development', function() {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
