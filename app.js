1685987c8b1984f82a11b85a425281f4c9920cf0
Peter Cottle <petermcottle@gmail.com>
2012-10-14 22:13:10 -0700

    big routing update

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var express = require('express');
+var routes = require('./routes');
+
+// connect to our DB
+var url = process.env.REDISTOGO_URL ||
+  'redis://petermcottle:36aa6d00c86319b03eb2e2f79cc7573f@drum.redistogo.com:9958/';
+var redis = require('redis-url').connect(url);
 
 var app = express.createServer();
+
 app.configure('development', function() {
-  // errors
   app.use(express.logger());
   app.use(express.errorHandler({
     dumpExceptions: true,
     showStack: true
   }));
 
-  // static
   app.use('/lib/', express.static(__dirname + '/lib'));
   app.use('/css/', express.static(__dirname + '/css'));
 
@@ -22,14 +27,8 @@ app.configure('development', function() {
 });
 
 
-app.get('/', function(request, response) {
-  response.render('index', {
-    locals: {
-      title: 'New Product Development!',
-      time: new Date().toString()
-    }
-  });
-});
+app.get('/', routes.index);
+app.get('/about', routes.about);
 
 var port = process.env.PORT || 3600;
 app.listen(port, function() {
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+
+exports.index = function(request, response) {
+  response.render('index');
+};
+
+exports.about = function(request, response) {
+  response.render('about');
+};
+
