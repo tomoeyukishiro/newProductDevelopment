ad57669600f1bc7b1c6afe12720d6d4d141cc0e9
Peter Cottle <petermcottle@gmail.com>
2012-11-06 12:06:39 -0800

    port option too

--- texting/index.js
+++ texting/index.js
@@ -5,7 +5,10 @@ var client = exports.client = new TwilioClient(
   'AC0097e3ebc6e9c4f7f1fc0fe963ef729c',
   'ac6b5d8f13c3f81753a613f623eb3d9a',
   'radiant-atoll-9524.herokuapp.com/',
-  { app: app }
+  {
+    app: app,
+    port: process.en.port || 3600
+  }
 );
 
 var phone = exports.phone = client.getPhoneNumber('+14085969236');
