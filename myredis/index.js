ebe0f2e340e1ceaa6e4bbaf3a2ff4c89aec2a0d7
Peter Cottle <petermcottle@gmail.com>
2012-11-07 13:33:54 -0800

    same db always

--- myredis/index.js
+++ myredis/index.js
@@ -1,5 +1,8 @@
-var url = process.env.REDISTOGO_URL ||
+/*var url = process.env.REDISTOGO_URL ||
   'redis://petermcottle:36aa6d00c86319b03eb2e2f79cc7573f@drum.redistogo.com:9958/';
+*/
+// use same url always
+var url = 'redis://petermcottle:36aa6d00c86319b03eb2e2f79cc7573f@drum.redistogo.com:9958/';
 
 exports.redis = require('redis-url').connect(url);
 
