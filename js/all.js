0971b0c45b882883955378db0c12eb4c956b163d
Peter Cottle <petermcottle@gmail.com>
2012-11-10 11:29:29 -0800

    refactor

---
+++ js/all.js
@@ -0,0 +1,7 @@
+$(document).delegate('#makebutton', 'click', function() {
+  $('#make_plant_form').submit();
+});
+
+$(document).delegate('#deletebutton', 'click', function() {
+  $('#delete_form').submit();
+})
